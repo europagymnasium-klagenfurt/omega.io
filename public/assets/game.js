aab6ab83ffdaf32836f16be6233470cd76959c36
Weissel Thomas <valueerrorx@users.noreply.github.com>
2026-06-10 11:03:32 +0000

    auto und steuerung hinzugefügt

--- public/assets/game.js
+++ public/assets/game.js
@@ -5,6 +5,8 @@
 // und die position des autos von anderen Spielern empfangen können
 
 
+const socket = io(); 
+
 class OmegaIo extends Phaser.Scene {
     preload() {
         this.load.image("racetrack", "assets/racetrack.jpg");
@@ -13,10 +15,36 @@ class OmegaIo extends Phaser.Scene {
 
     create() {
         this.add.tileSprite(0, 0, 1200, 800, "racetrack").setOrigin(0, 0);
+
+        this.car = this.physics.add.sprite(400, 300, 'car').setCollideWorldBounds(true).setScale(0.5);
+        this.car.setAngle(90);
+        this.car.body.allowRotation = false;
+        this.car.setCircle(50, 25, 55);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
 
-    update() {
-        // hier können wir die position des autos aktualisieren und an den server senden
+    update(time, delta) {
+        // Bewegung des Autos mit den Pfeiltasten. links und rechts rotieren die Grafik, die Hitbox bleibt axis-aligned.
+        const rotationSpeed = 150;
+        const speed = 300;
+
+        if (this.cursors.left.isDown) {
+            this.car.angle -= rotationSpeed * delta / 1000;
+        } else if (this.cursors.right.isDown) {
+            this.car.angle += rotationSpeed * delta / 1000;
+        }
+
+        const angle = this.car.rotation - Math.PI / 2;
+       
+        if (this.cursors.up.isDown) {
+            this.physics.velocityFromRotation(angle, speed, this.car.body.velocity);
+        } else if (this.cursors.down.isDown) {
+            this.physics.velocityFromRotation(angle, -speed, this.car.body.velocity);
+        } else {
+            this.car.body.velocity.scale(0.95);
+        }
+
     }
 
 }
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const httpServer = createServer(app);
 const io = new Server(httpServer);
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  console.log("a user connected" +  socket.id);
 
   socket.on("disconnect", () => {
     console.log("user disconnected");
